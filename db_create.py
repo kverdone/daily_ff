7b0d378e64987b03db53892ae51362b8d6ce88ef
kverdone <kverdone@gmail.com>
2015-10-01 00:37:59 -0700

    Get Postgres working on Heroku

--- db_create.py
+++ db_create.py
@@ -5,6 +5,7 @@ db.create_all()
 
 db.session.add(Post('Good', 'This is good.'))
 db.session.add(Post('App', 'Making an app!'))
+db.session.add(Post('Postgres', 'Now we are running with postgres'))
 
 
 db.session.commit()
